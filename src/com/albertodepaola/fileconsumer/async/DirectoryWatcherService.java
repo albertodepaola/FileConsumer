5059236cb7e1a700500f70ed86d704ff1bdc354e
Alberto de Paola <albertodepaola@gmail.com>
2018-02-27 09:04:34 -0300

    Adding comments and documentation

--- src/com/albertodepaola/fileconsumer/async/DirectoryWatcherService.java
+++ src/com/albertodepaola/fileconsumer/async/DirectoryWatcherService.java
@@ -20,6 +20,14 @@ import java.util.concurrent.Future;
 
 import com.albertodepaola.fileconsumer.model.Tuple;
 
+/**
+ * Serviço feitor para observar um diretorio por arquivos com determinada
+ * extensão, processar eles com o ABParser e mover eles para um diretorio de
+ * saida.
+ * 
+ * @author alberto
+ *
+ */
 public class DirectoryWatcherService {
 
 	private static final String ACCEPTED_EXTENSION_ERROR = ".error.dat";
@@ -30,13 +38,26 @@ public class DirectoryWatcherService {
 	private ExecutorService executor;
 	private WatchService watcher;
 
+	/**
+	 * Construtor que toma o direotorio à verificar e o diretorio de saida
+	 * 
+	 * @param dirIn
+	 *            onde será registrado o watchService
+	 * @param dirOut
+	 *            onde serão colocados os arquivos de saida
+	 */
 	public DirectoryWatcherService(Path dirIn, Path dirOut) {
 		this.dirIn = dirIn;
 		this.dirOut = dirOut;
 	}
 
+	/**
+	 * Função que inicia o serviço e devolve o controle
+	 */
 	public void start() {
 
+		// cria um Executor de uma thread, para independizar o serviço de
+		// verificação e devolver o controle
 		if (executor == null) {
 			executor = Executors.newSingleThreadExecutor();
 		}
@@ -47,10 +68,17 @@ public class DirectoryWatcherService {
 
 	}
 
+	/**
+	 * Esta função registra um watchService no dirIn para verificar somente a
+	 * criação de arquivos com a extensão ACCEPTED_EXTENSION. Adicionalmente,
+	 * verifica a existencia de arquivos no diretorio antes de começar. Cada
+	 * arquivo e procesado pelo parser em paralelo, e quando finalizado é movido
+	 * para o dirOut, também em paralelo.
+	 */
 	private void watchDirectory() {
 		try {
-			watcher = FileSystems.getDefault().newWatchService();
 
+			// Primeiro verificar os arquivos já existentes no diretorio
 			List<Future<Tuple<String, File>>> results = new ArrayList<>();
 			String[] list = dirIn.toFile().list(new FilenameFilter() {
 
@@ -60,24 +88,31 @@ public class DirectoryWatcherService {
 				}
 			});
 			for (String filename : list) {
-				System.out.println("Filename: " + filename);
 				Future<Tuple<String, File>> existingFile = FileExecutorService
 						.submitFile(dirIn.resolve(filename).toFile());
-				
+
 				results.add(existingFile);
 			}
 
+			// Processar arquivos existentes
 			processResults(results);
 
+			// Criar watcher com variable de tipo instancia para poder para o
+			// serviço mais tarde
+			watcher = FileSystems.getDefault().newWatchService();
 			dirIn.register(watcher, ENTRY_CREATE);
 
 			WatchKey key;
 
+			// enquanto não tem nada, aguarda
 			while ((key = watcher.take()) != null) {
+				// assim que recupera uma lista de eventos, realiza o reset para
+				// poder escutar novos. Caso contrario novos arquivos se perdem
 				key.reset();
 
 				for (WatchEvent<?> event : key.pollEvents()) {
 
+					// o .context() contem o nome do arquivo
 					if (!event.context().toString().endsWith(ACCEPTED_EXTENSION)) {
 						continue;
 					}
@@ -85,6 +120,7 @@ public class DirectoryWatcherService {
 					@SuppressWarnings("unchecked")
 					WatchEvent<Path> ev = (WatchEvent<Path>) event;
 
+					// envia o arquivo para ser parseado e guarda um futuro
 					Future<Tuple<String, File>> submitFile = FileExecutorService
 							.submitFile(dirIn.resolve(ev.context()).toFile());
 
@@ -92,6 +128,7 @@ public class DirectoryWatcherService {
 
 				}
 
+				// Processar arquivos novos
 				processResults(results);
 
 			}
@@ -103,21 +140,28 @@ public class DirectoryWatcherService {
 		}
 	}
 
+	/**
+	 * Para cada objeto da lista de resultados, verifica o status e move o
+	 * arquivo para o diretorio de saida.
+	 * 
+	 * @param results
+	 */
 	private void processResults(List<Future<Tuple<String, File>>> results) {
-		
+
+		// para cada resultado, executa em paralelo, já que a ordem da lista não
+		// define a ordem do retorno de future.get()
 		results.parallelStream().forEach(future -> {
 			try {
-				
+
+				// quando ainda não finalizou o parse, fica aguardando aqui
 				Tuple<String, File> result = future.get();
-				
+
 				if ("OK".equals(result.x)) {
-					Files.move(result.y.toPath(),
-							dirOut.resolve(result.y.toPath().getFileName().toString().replace(ACCEPTED_EXTENSION, ACCEPTED_EXTENSION_DONE)),
-							REPLACE_EXISTING);
+					Files.move(result.y.toPath(), dirOut.resolve(result.y.toPath().getFileName().toString()
+							.replace(ACCEPTED_EXTENSION, ACCEPTED_EXTENSION_DONE)), REPLACE_EXISTING);
 				} else {
-					Files.move(result.y.toPath(),
-							dirOut.resolve(result.y.toPath().getFileName().toString().replace(ACCEPTED_EXTENSION, ACCEPTED_EXTENSION_ERROR)),
-							REPLACE_EXISTING);
+					Files.move(result.y.toPath(), dirOut.resolve(result.y.toPath().getFileName().toString()
+							.replace(ACCEPTED_EXTENSION, ACCEPTED_EXTENSION_ERROR)), REPLACE_EXISTING);
 				}
 			} catch (Exception e) {
 				throw new IllegalStateException(e);
@@ -125,6 +169,12 @@ public class DirectoryWatcherService {
 		});
 	}
 
+	/**
+	 * Permite parar o serviço de verificação
+	 * 
+	 * @throws InterruptedException
+	 * @throws IOException
+	 */
 	public void stop() throws InterruptedException, IOException {
 		if (watcher != null)
 			watcher.close();
